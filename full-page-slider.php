c3ef15e46b89c8e866cb9a64b759a0b807bb1614
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-01-11 11:09:04 +0300

    Modify the plugin header details.

--- full-page-slider.php
+++ full-page-slider.php
@@ -1,7 +1,8 @@
+<?php
 /*
 Plugin Name: Full Page Slider
 Plugin URI: https://mwororokevin.github.io
-Description: A WordPress Slider that allows the slider to occupy the full page height on all screen sizes.
+Description: An Image Slider Plugin that allows the user to create image sliders that can cover the full page height on all screen sizes.
 Author: Kevin Mwororo
 Author URI: https://mwororokevin.github.io
 Text Domain: full-page-slider
