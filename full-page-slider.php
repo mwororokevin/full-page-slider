9e102d80154214d40bb2c44370c4a8aba95751b0
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-01-11 11:13:17 +0300

    Modify the plugin header.
    It will add the PHP version requirement, WordPress version and add the
    license and license URI.

--- full-page-slider.php
+++ full-page-slider.php
@@ -5,7 +5,11 @@ Plugin URI: https://mwororokevin.github.io
 Description: An Image Slider Plugin that allows the user to create image sliders that can cover the full page height on all screen sizes.
 Author: Kevin Mwororo
 Author URI: https://mwororokevin.github.io
+Requires PHP: 7.3.1
+Requires at least: 5.6
 Text Domain: full-page-slider
 Domain Path: /languages/
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Version: 1.0.0
 */
