757c9f0c06ec40ee794f46d3adf4219d8d1018b0
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-01-12 11:36:52 +0300

    Adding the plugin as a menu item to the WordPress Admin Sidebar

--- full-page-slider.php
+++ full-page-slider.php
@@ -13,3 +13,19 @@ License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Version: 1.0.0
 */
+
+add_action( 'admin_menu', 'fps_admin_menu_setup' );
+// Adding the plugin as a menu item to the WordPress Admin Sidebar
+function fps_admin_menu_setup () {
+    add_menu_page(
+        'Full Page Slider Page',
+        'Full Page Slider',
+        'manage_options',
+        'fullpageslider',
+        'fps_page_html'
+    );
+}
+
+function fps_page_html (){
+    echo "<h1>Hello World!</h1>";
+}
